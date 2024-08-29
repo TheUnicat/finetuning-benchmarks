96c1a2099fdbf25388e0063196aaa66fda07bcbe
TheUnicat <matthew4680@icloud.com>
2024-08-29 13:35:26 -0700

    refactor results folder

--- benchmark_llama8_base.py
+++ benchmark_llama8_base.py
@@ -1,20 +1,34 @@
 import json
 from typing import List, Dict, Any
+from gradio_client import Client
+
+client = Client("https://576419700ef8950c0e.gradio.live/")
 
 # always use type hinting
+# only load up to 500 samples
 
-
-# only load up to500 samples
 with open('data/gsm8k/test.jsonl', 'r') as f:
-    samples = f.readlines()[:500]
+    samples = f.readlines()[:2]
 
 CoT_prompt = "Let's think step by step. At the end, you MUST write the answer as an integer after '####'."
 
 def query_llama(sample: str) -> str:
+    result = client.predict(
+        message=sample,
+        api_name="/chat"
+    )
+    return result
 
-
-def test_sample(sample: str, answer: str) -> bool:
+def test_sample(sample: str, answer: str) -> None:
     answer_num: int = int(answer.split("####")[-1].strip())
+    llama_answer: str = query_llama(sample)
+    correct: bool = int(llama_answer.split["####"][-1].strip()) == answer_num
+
+    # save sample
+    with open("results/gsm8k_llama3_8b_instruct_base.jsonl", "a") as f:
+        # the file is an array of objects
+        f.write(json.dumps({"sample": sample, "answer": answer, "correct": correct}) + "\n")
+    
     
 
 
